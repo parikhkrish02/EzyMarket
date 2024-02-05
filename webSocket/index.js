a663c500f8546b1cc6d7b72a7b344a947d3da6ae
krish <parikhkrish01@gmail.com>
2024-02-06 01:14:05 +0530

    fix(socket, frontend): socket endpoints added and disconnect trigger

--- webSocket/index.js
+++ webSocket/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
         methods: ["GET", "POST"]
     }
 })
@@ -21,7 +21,12 @@ io.on("connection", (socket) => {
         console.log("update fires");
         socket.broadcast.emit("update")
     })
+
+    socket.on("disconnect", (reason) => {
+        console.log('-------------------------------------------------------------------------------------------------------------------------------------');
+      });
 })
+
 
 server.listen(3001, () => {
     console.log('Socket Server Running !!!');
